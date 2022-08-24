1832eaac3902763145fcc0ee9b16958ad177e4a1
Khaizad52 <1khairulh@gmail.com>
2022-08-24 13:34:52 +0800

    2

--- src/main/java/SSF/mini_project/controller/NewsController.java
+++ src/main/java/SSF/mini_project/controller/NewsController.java
@@ -1,9 +1,14 @@
 package SSF.mini_project.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import SSF.mini_project.models.News;
 import SSF.mini_project.service.NewsService;
 
 @Controller
@@ -13,5 +18,9 @@ public class NewsController {
     private NewsService newsService;
     
     @GetMapping
-    
+    public String newsPage(Model model){
+        News news = new News();
+        
+        
+    }
 }
